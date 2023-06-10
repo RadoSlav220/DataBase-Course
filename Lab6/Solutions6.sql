777c934393bd84c6ecd71da067ddcf46ef49b28a
RadoSlav <radoslav4096@gmail.com>
2023-06-10 16:30:40 +0300

    Complete tasks for CASE in lab6

--- Lab6/Solutions6.sql
+++ Lab6/Solutions6.sql
@@ -14,4 +14,17 @@ group by case
 			when year(birthdate) < 1980 then '70s'
 			else 'during after 80s'
 		 end;
+
+use ships;
+
+-- Task 2.1
+select battle
+from outcomes
+join ships on ship = name
+join classes on ships.class = classes.class
+where numguns < 9
+group by battle
+having count(*) >= 3 and sum(case result when 'ok' then 1 else 0 end) >= 2;
+
+
 					
